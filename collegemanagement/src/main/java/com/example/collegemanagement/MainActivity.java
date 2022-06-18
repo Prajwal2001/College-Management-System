838c48243920fc01e91556522d7846854edeb4c1
Prajwal Kulkarni <kulkarniprajwal.01@gmail.com>
2022-06-18 23:05:48 +0530

    Adding Login feature

--- collegemanagement/src/main/java/com/example/collegemanagement/MainActivity.java
+++ collegemanagement/src/main/java/com/example/collegemanagement/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.collegemanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login_btn);
+        login.setOnClickListener(view -> {
+            Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show();
+        });
     }
 }
