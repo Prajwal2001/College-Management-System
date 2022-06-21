727865e67433d3d8f026a4faf766ce9af56c3315
Prajwal Kulkarni <kulkarniprajwal.01@gmail.com>
2022-06-21 17:25:53 +0530

    Added firestore to project

--- collegemanagement/src/main/java/com/example/collegemanagement/MainActivity.java
+++ collegemanagement/src/main/java/com/example/collegemanagement/MainActivity.java
@@ -8,12 +8,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +27,7 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     Button login;
+    EditText email, pwd;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
@@ -28,27 +35,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        email = findViewById(R.id.emailId);
+        pwd = findViewById(R.id.password);
         login = findViewById(R.id.login_btn);
+
         login.setOnClickListener(view -> {
-            Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show();
+            System.out.println(email.getText().toString());
+            Toast.makeText(this, email.getText().toString(), Toast.LENGTH_SHORT).show();
+
+            // Create a new user with a first and last name
+//            Map<String, String> user = new HashMap<>();
+//            user.put("email", email.getText().toString());
+//            user.put("password", pwd.getText().toString());
+//
+//
+//            // Add a new document with a generated ID
+//            db.collection("users")
+//                    .get()
+//                    .addOnSuccessListener(documentReference -> System.out.println("DocumentSnapshot added with ID: " + documentReference.getId()))
+//                    .addOnFailureListener(e -> Log.d("jo_mama", e.toString()));
+
+            db.collection("users").get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult())
+                        Log.d("DATA", document.getId() + " \n\n\n\n=> " + document.getData());
+                } else Log.d("Get failed: ", "" + task.getException());
+            });
         });
-
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-        // Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId()))
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
     }
-
 }
