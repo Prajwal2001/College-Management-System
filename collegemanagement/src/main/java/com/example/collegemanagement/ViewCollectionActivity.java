926570e3448fbe399ed66216a6e85bdec3901377
Akshay <akshayrgowda2001@gmail.com>
2022-06-22 18:40:31 +0530

    View Collection: List View Basic Data Output

--- collegemanagement/src/main/java/com/example/collegemanagement/ViewCollectionActivity.java
+++ collegemanagement/src/main/java/com/example/collegemanagement/ViewCollectionActivity.java
@@ -1,7 +1,5 @@
 package com.example.collegemanagement;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,11 +7,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,28 +33,25 @@ public class ViewCollectionActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.lv);
 
-        db.collection(collection).addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
-                if (e != null)
-                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                list.clear();
-                for (DocumentSnapshot doc : documentSnapshots) {
-                    Map<String, Object> data = doc.getData();
-                    String item = new String();
-                    for (Map.Entry<String, Object> entry : data.entrySet()) {
-                        item = item.concat(entry.getKey()+": "+entry.getValue()+"\n");
-                    }
-                    list.add(item);
+        db.collection(collection).addSnapshotListener((documentSnapshots, e) -> {
+            if (e != null)
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            list.clear();
+            for (DocumentSnapshot doc : documentSnapshots) {
+                Map<String, Object> data = doc.getData();
+                String item = "";
+                for (Map.Entry<String, Object> entry : data.entrySet()) {
+                    item = item.concat(entry.getKey()+": "+entry.getValue()+"\n");
                 }
-                Log.d("List View", "onEvent: " + list.toString());
-
-                ArrayAdapter arrayAdapter = new ArrayAdapter<String>(ViewCollectionActivity.this,
-                        R.layout.view_list_item,
-                        list);
-                listView.setAdapter(arrayAdapter);
-
+                list.add(item);
             }
+            Log.d("List View", "onEvent: " + list.toString());
+
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(ViewCollectionActivity.this,
+                    R.layout.view_list_item,
+                    list);
+            listView.setAdapter(arrayAdapter);
+
         });
     }
 }
