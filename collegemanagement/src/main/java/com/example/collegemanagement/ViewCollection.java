cb33a746b443cc6a911d66d55a9116913d9d4934
Akshay <akshayrgowda2001@gmail.com>
2022-06-24 15:16:11 +0530

    ViewActivity: Delete Button

--- collegemanagement/src/main/java/com/example/collegemanagement/ViewCollection.java
+++ collegemanagement/src/main/java/com/example/collegemanagement/ViewCollection.java
@@ -3,8 +3,6 @@ package com.example.collegemanagement;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,7 +13,6 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class ViewCollection extends AppCompatActivity {
