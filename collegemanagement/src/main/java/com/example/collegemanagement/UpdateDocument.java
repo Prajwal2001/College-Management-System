bfaa09742d13d83321148ed23560e6bb1c26e7b7
Akshay <akshayrgowda2001@gmail.com>
2022-07-09 14:44:17 +0530

    UpdateDocuments: :(

--- collegemanagement/src/main/java/com/example/collegemanagement/UpdateDocument.java
+++ collegemanagement/src/main/java/com/example/collegemanagement/UpdateDocument.java
@@ -1,9 +1,16 @@
 package com.example.collegemanagement;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +21,26 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Map;
+
+class UpdateDocAdapter extends ArrayAdapter<Map.Entry<String, String>> {
+    private static final String LOG_TAG = UpdateDocAdapter.class.getSimpleName();
+
+    public UpdateDocAdapter(Activity context, ArrayList<Map.Entry<String, String>> ViewListItems) {
+        super(context, 0, ViewListItems);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Map.Entry<String, String> entry = getItem(position);
+
+        TextView fieldTextView = (TextView) convertView.findViewById(R.id.update_list_text);
+        EditText fieldEditText = (EditText) convertView.findViewById(R.id.update_list_edit);
+        Toast.makeText(getContext(), entry.toString(), Toast.LENGTH_SHORT).show();
+        fieldTextView.setText(entry.getKey());
+        fieldEditText.setText(entry.getValue());
+        return convertView;
+    }
+}
 
 public class UpdateDocument extends AppCompatActivity {
 
@@ -29,18 +56,20 @@ public class UpdateDocument extends AppCompatActivity {
         String collection = update.getStringExtra("collection");
         String docId = update.getStringExtra("docId");
 
-        updateListView = findViewById(R.id.updateList);
-        updateViewList.clear();
+        updateListView = (ListView) findViewById(R.id.updateList);
+        //updateViewList.clear();
+
         db.collection(collection).document(docId).get().addOnCompleteListener(task -> {
             if (task.isSuccessful()){
                 DocumentSnapshot doc = task.getResult();
                 Map<String, Object> data = doc.getData();
                 assert data != null;
-                for (Map.Entry<String, Object> entry: data.entrySet())
-                    updateViewList.add(new AbstractMap.SimpleEntry<>(entry.getValue().toString(), entry.getKey()));
-                Toast.makeText(getApplicationContext(), doc.toString(), Toast.LENGTH_SHORT).show();
-//                    ArrayAdapter updateListAdapter = new ArrayAdapter(UpdateDocument.this, updateViewList);
-//                    updateListView.setAdapter(updateListAdapter);
+                for (Map.Entry<String, Object> entry: data.entrySet()) {
+                    updateViewList.add(new AbstractMap.SimpleEntry<String, String>(entry.getKey(), entry.getValue().toString()));
+                    Log.d("UpdtVal", "getView: "+ entry.getKey() + " " + entry.getValue());
+                }
+                UpdateDocAdapter updateListAdapter = new UpdateDocAdapter(UpdateDocument.this, updateViewList);
+                updateListView.setAdapter(updateListAdapter);
             }
             else {
                 Log.d("GetDoc", "Get Failed with ", task.getException());
